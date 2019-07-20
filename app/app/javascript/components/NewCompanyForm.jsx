67ac5838e24069f2d2652c5439a965f749591a78
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-20 13:59:23 -0600

    added form inputs

--- app/app/javascript/components/NewCompanyForm.jsx
+++ app/app/javascript/components/NewCompanyForm.jsx
@@ -6,15 +6,49 @@ class NewCompanyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      companyName: '',
+      city: '',
+      state: '',
+      date: '',
+      description: '',
     };
   }
 
   render() {
+    const {
+      companyName, city, state, date, description,
+    } = this.state;
     return (
       <React.Fragment>
         <Navbar />
         <form>
           <h1>Add New Company</h1>
+          <label htmlFor="companyName">
+            Company Name:
+            <input type="text" name="companyName" value={companyName} />
+          </label>
+
+          <label htmlFor="city">
+            City:
+            <input type="text" name="city" value={city} />
+          </label>
+
+          <label htmlFor="state">
+            State:
+            <input type="text" name="state" value={state} />
+          </label>
+
+          <label htmlFor="date">
+            Founded Date:
+            <input type="date" name="date" value={date} />
+          </label>
+
+          <label htmlFor="description">
+            description:
+            <textarea name="description" value={description} />
+          </label>
+
+          <input type="submit" value="Save" />
         </form>
       </React.Fragment>
     );
