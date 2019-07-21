942e04cbe83ccac1133c27b723e50c90b3ec8d09
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-20 20:15:24 -0600

    added edit modal

--- app/app/javascript/components/CompanyProfile.jsx
+++ app/app/javascript/components/CompanyProfile.jsx
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from 'react';
 import Navbar from '../templates/Navbar';
 import companyType from '../types/company';
 import { formatYMD } from '../helpers/date';
+import Modal from './profileComponents/EditModal';
 
 class CompanyProfile extends Component {
   static deleteProject(id) {
@@ -29,12 +30,13 @@ class CompanyProfile extends Component {
       city,
       state,
       description,
+      modal: true,
     };
   }
 
   render() {
     const {
-      id, founded_date, name, city, state, description,
+      id, founded_date, name, city, state, description, modal,
     } = this.state;
 
     const formatedDate = founded_date !== null ? formatYMD(founded_date) : '';
@@ -42,13 +44,16 @@ class CompanyProfile extends Component {
     return (
       <Fragment>
         <Navbar />
+
+        {modal ? <Modal /> : null}
+
         <div className="company-profile">
           <h1>{name}</h1>
 
           <div className="row">
             <p>{formatedDate}</p>
             <p>{`${city}, ${state}`}</p>
-            <button type="button">
+            <button type="button" onClick={() => { this.setState({ modal: !modal }); }}>
               Edit
             </button>
             <button type="button" onClick={() => CompanyProfile.deleteProject(id)}>
---
+++ app/app/javascript/components/profileComponents/EditModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = () => (
+  <div className="modal">
+    <h1>MODAL LOGIC!!</h1>
+  </div>
+);
+
+export default Modal;
